d0b71e046d8ef0824222579820d20beac7f54f1e
Sarthak-Kulkarni <35108485+Sarthak-Kulkarni@users.noreply.github.com>
2026-08-11 22:37:36 +0530

    Add: k-closest-points-to-origin - submission-0

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-0.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+
+        PriorityQueue<int[]> heap = new PriorityQueue<>((a, b) -> ((b[0]*b[0] + b[1]*b[1]) - (a[0]*a[0] + a[1]*a[1])));
+        
+        for(int[] point: points) {    
+            heap.offer(point);
+
+            if(heap.size() > k) {
+                heap.poll();
+            }
+        }
+
+        int[][] res = new int[k][2];
+        int idx = 0;
+        while(!heap.isEmpty()) {
+            res[idx++] = heap.poll();
+        }
+
+        return res;
+    }
+}
